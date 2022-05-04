24e88b50ecc99b75910433c137824cb160ab9690
Andrei Lakotko <ejakaa@gmail.com>
2022-05-04 12:27:15 +0400

    Delete wrong-platform-error.js

--- assert-view/errors/wrong-platform-error.js
+++
@@ -1,9 +0,0 @@
-module.exports = class WrongPlatformError extends Error {
-	constructor (message) {
-		super();
-
-		this.name = this.constructor.name;
-		this.message =
-			message || 'Wrong platform, assertMobileView works only for mobile apps';
-	}
-};
